e16c72c9b57886b89b44b7110579c3445b50bafa
mnaslinak <mnaslinak@gmail.com>
2026-04-29 21:15:22 +0530

    🗃️ front-end-day-2-display

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ function App() {
 
   return (
     <>
-      <h1 className='text-[#995422] text-9xl bg-amber-200'> Hello PC world </h1>
-      <Productcard name="macbook" price="Lkr 245000" photo="https://images.unsplash.com/photo-1605360846374-3ae595056068?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></Productcard>
-      <Productcard name="Dell" price="Lkr 242000" photo="https://images.unsplash.com/photo-1694869590089-3e7f02fbc705?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D "></Productcard>
-      <Productcard name="Lenovo" price="Lkr 255000" photo="https://images.unsplash.com/photo-1610567177680-033d1d9198bc?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></Productcard>
-      
-    </>
+    <div className='w-150 h-150 bg-amber-700 flex justify-center items-center'>
+      <div className='w-130 h-130 bg-gray-700 flex flex-col items-center justify-evenly'>
+      <div className='w-30 h-30 bg-blue-600'></div>
+      <div className='w-30 h-30 bg-red-600'></div>
+      <div className='w-30 h-30 bg-green-600'></div>
+      <div className='w-30 h-30 bg-white'></div>
+      </div>
+      </div>
+      </>
   )
 }
 
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,10 +1,11 @@
-export default function Productcard(props){
+// export default function Productcard(props){
 
-    return(
-            <div className="border w-56 h-80">
-            <h1>{props.name}</h1>
-            <img src={props.photo} alt="" className="w-40 h-40" />
-            <p>{props.price}</p>
-            </div>
-          )
-}
+//     return(
+//             <div className="border w-56 h-80">
+//             <h1>{props.name}</h1>
+//             <img src={props.photo} alt="" className="w-40 h-40" />
+//             <p>{props.price}</p>
+//             </div>
+//           )
+// }
+
